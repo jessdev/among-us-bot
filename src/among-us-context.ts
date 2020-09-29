906537e48a51cbfab599db7f599afe4aad543993
jessdev <jessdev@umich.edu>
2020-09-29 06:32:33 -0400

    added more tests

--- src/among-us-context.ts
+++ src/among-us-context.ts
@@ -2,14 +2,27 @@ import sqlite3 from "sqlite3";
 import { CrewMateRow, GameRow, ParticipantRow } from "./databaseModels/database-index";
 
 export class AmongUsContext {
-    private database: sqlite3.Database
+    private database: sqlite3.Database;
+    public databaseSetup: Promise<void> = new Promise((resolve, reject) => {/*intentionally left blank */});
     constructor(databaseFileName: string) {
         this.database = new sqlite3.Database(databaseFileName);
-        this.database.run("CREATE TABLE IF NOT EXISTS crewMateType(id INTEGER PRIMARY KEY, type TEXT)");
-        this.database.run("CREATE TABLE IF NOT EXISTS crewMate(id INTEGER PRIMARY KEY AUTOINCREMENT, discord TEXT)");
-        this.database.run("CREATE TABLE IF NOT EXISTS deathType(id INTEGER PRIMARY KEY, type TEXT)");
-        this.database.run("CREATE TABLE IF NOT EXISTS game(id INTEGER PRIMARY KEY AUTOINCREMENT, winnerTypeId INTEGER)");
-        this.database.run("CREATE TABLE IF NOT EXISTS participant(id INTEGER PRIMARY KEY AUTOINCREMENT, gameId INTEGER, crewmateId INTEGER, deathtypeId INTEGER)");
+        this.databaseSetup = this.SetUp(databaseFileName);
+    }
+
+    public async SetUp(databaseFileName: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.database.run("CREATE TABLE IF NOT EXISTS crewMateType(id INTEGER PRIMARY KEY, type TEXT)", this.resolvePromise(reject, () =>{
+                this.database.run("CREATE TABLE IF NOT EXISTS crewMate(id INTEGER PRIMARY KEY AUTOINCREMENT, discord TEXT)", this.resolvePromise(reject, () => {
+                    this.database.run("CREATE TABLE IF NOT EXISTS deathType(id INTEGER PRIMARY KEY, type TEXT)", this.resolvePromise(reject, () => {
+                        this.database.run("CREATE TABLE IF NOT EXISTS game(id INTEGER PRIMARY KEY AUTOINCREMENT, winnerTypeId INTEGER)", this.resolvePromise(reject, () => { 
+                            this.database.run("CREATE TABLE IF NOT EXISTS participant(id INTEGER PRIMARY KEY AUTOINCREMENT, gameId INTEGER, crewmateId INTEGER, deathtypeId INTEGER)", this.resolvePromise(reject, () => { 
+                                resolve();
+                            }));
+                        }));
+                    }));
+                }));
+            }));
+        });
     }
 
     public async addGame(game: GameRow): Promise<number> {
@@ -43,23 +56,30 @@ export class AmongUsContext {
         });
     }
 
+    public async addCrewMate(crewmate: CrewMateRow) {
+        return await new Promise(async (resolve, reject) => {
+            this.database.run('INSERT INTO crewMate(discord) VALUES (?)', [crewmate.discordId], this.resolvePromise(reject, async (data: any) => {
+                if(data !== undefined && data !== null) {
+                    resolve(data);
+                    return;
+                }
+                resolve();
+            }))
+        });
+    }
+
     public async getUser(discord: string): Promise<CrewMateRow> {
         return await new Promise((resolve, reject) => {
-            this.database.run("SELECT * From CrewMate Where discord = '?'", 
-                discord, 
+            this.database.get("SELECT * From crewMate Where discord = '?name'", 
+                {
+                    $name: discord
+                },
                 this.resolvePromise(reject, (data:any) => {
-                    try{
-                        console.log(data);
-                        if(data === undefined){
-                            resolve({} as CrewMateRow);
-                        }
-                        if(data.length > 0){
-                            resolve(new CrewMateRow(data[0].Id, data[0].discordId));
-                        }
-                        resolve({} as CrewMateRow);
-                    } catch {
-                        resolve({} as CrewMateRow);
+                    if(data !== undefined && data !== null) {
+                        resolve(data);
+                        return;
                     }
+                    reject();
             }));
         });
     }
--- test/context.test.ts
+++ test/context.test.ts
@@ -1,14 +1,46 @@
+import { v4 as Guid} from "uuid";
 import { AmongUsContext } from "../src/among-us-context";
+import { CrewMateRow } from "../src/databaseModels/crewmate-row";
+import { ParticipantRow } from "../src/databaseModels/database-index";
 import { GameRow } from "../src/databaseModels/game-row";
 
-var testContext = new AmongUsContext("testdatabase.db");
+const guid: string = Guid();
+var testContext = new AmongUsContext(guid+".db");
 
 test('save game to database', async () => {
+    await testContext.databaseSetup;
     try {
         var gamerow = new GameRow(0, 0);
         var result = await testContext.addGame(gamerow);
-        expect(result).toBe(0);
+        expect(result).toBe(1);
     } catch(error) {
+        console.log(error);
+        throw error;
+    }
+});
+
+test('save crewmate to database', async () => {
+    await testContext.databaseSetup;
+    try {
+        let discord = "test";
+        let crew = new CrewMateRow(0, discord);
+        var result = await testContext.addCrewMate(crew);
+        expect(result).toBe(undefined);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+});
+
+test('get crewmate', async () => {
+    await testContext.databaseSetup;
+    try {
+        let discord = "testUser";
+        let crew = new CrewMateRow(0, discord);
+        await testContext.addCrewMate(crew);
+        let result = await testContext.getUser(discord);
+        expect(result.discordId).toBe(discord);
+    } catch (error) {
         console.log(error);
         throw error;
     }
